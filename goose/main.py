f8187742b64dfc4b9e317e5be07ca922a1146c10
anzhelika17 <akostyuk@conestogac.on.ca>
2023-08-11 16:08:32 -0400

    Added enemies and bonuses

--- goose/main.py
+++ goose/main.py
@@ -1,5 +1,7 @@
+import random
 import pygame
 import sys
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
 
@@ -8,39 +10,99 @@ HEIGHT = 800
 WIDTH = 1200
 PLAYER_COLOR = (255, 255, 255)
 DISPLAY_COLOR = (0, 0, 0)
+ENEMY_COLOR = (0, 0, 255)
+BONUS_COLOR = (255, 255, 0)
 
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Goose Game")
 
+#Player declaration#
 player_size = (20, 20)
 player = pygame.Surface(player_size)
 player.fill(PLAYER_COLOR)
 player_rect = player.get_rect()
-player_speed = [1, 1]
+# player_speed = [1, 1]
+player_move_down = [0, 1]
+player_move_up = [0, -1]
+player_move_left = [-1, 0]
+player_move_right = [1, 0]
+
+def create_bonus():
+    #Bonus declaration#
+    bonus_size = (30, 30)
+    bonus = pygame.Surface(bonus_size)
+    bonus.fill(BONUS_COLOR)
+    bonus_rect = pygame.Rect(random.randint(0, WIDTH), 0, *bonus_size)
+    bonus_move = [0, random.randint(1, 6)]
+
+    return [bonus, bonus_rect, bonus_move]
+
+CREATE_BONUS = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_BONUS, 2500)
+
+bonuses = []
+
+def create_enemy():
+    #Enemy declaration#
+    enemy_size = (30, 30)
+    enemy = pygame.Surface(enemy_size)
+    enemy.fill(ENEMY_COLOR)
+    enemy_rect = pygame.Rect(WIDTH, random.randint(0, HEIGHT), *enemy_size)
+    enemy_move = [random.randint(-6, -1), 0]
+
+    return [enemy, enemy_rect, enemy_move]
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, 1500)
+
+enemies = []
 
 while True: 
     FPS.tick(120)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy())
+        if event.type == CREATE_BONUS:
+            bonuses.append(create_bonus())
 
     main_display.fill(DISPLAY_COLOR)
-
-    #Player movements#
-    if player_rect.bottom >= HEIGHT:
-        player_speed[1] = -player_speed[1]
+    keys = pygame.key.get_pressed()
+    if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+        player_rect = player_rect.move(player_move_down)
     
-    if player_rect.right >= WIDTH:
-        player_speed[0] = -player_speed[0]
+    if keys[K_UP] and player_rect.top > 0:
+        player_rect = player_rect.move(player_move_up)
 
-    if player_rect.top < 0:
-        player_speed[1] = -player_speed[1]
+    if keys[K_RIGHT] and player_rect.right < WIDTH:
+        player_rect = player_rect.move(player_move_right)
     
-    if player_rect.left < 0:
-        player_speed[0] = -player_speed[0]
+    if keys[K_LEFT] and player_rect.left > 0:
+        player_rect = player_rect.move(player_move_left)
+
+    #Shows enemies#
+    for enemy in enemies:
+        enemy[1] = enemy[1].move(enemy[2])
+        main_display.blit(enemy[0], enemy[1])
+
+    #Shows bonuses#
+    for bonus in bonuses:
+        bonus[1] = bonus[1].move(bonus[2])
+        main_display.blit(bonus[0], bonus[1])
 
     main_display.blit(player, player_rect)
 
-    player_rect = player_rect.move(player_speed)
+    print(len(bonuses))
 
     pygame.display.flip()
+
+    #Removes enemies when they are out of screen#
+    for enemy in enemies:
+        if enemy[1].left < 0:
+            enemies.pop(enemies.index(enemy))
+
+    #Removes bonuses when they are out of screen#
+    for bonus in bonuses:
+        if bonus[1].top > HEIGHT:
+            bonuses.pop(bonuses.index(bonus))
